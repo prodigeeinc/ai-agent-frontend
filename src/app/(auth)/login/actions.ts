60dbc1e8e91ed01da724ad8b2d38e734c10eb225
alhassan50 <abdullahialhassanbaaba@gmail.com>
2025-10-29 20:31:49 +0000

    add zod schema to backend

--- src/app/(auth)/login/actions.ts
+++ src/app/(auth)/login/actions.ts
@@ -3,16 +3,20 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "@/lib/superbase/server";
+import { loginSchema } from "@/lib/zodSchemas";
 
 export async function login(formData: FormData) {
   const supabase = await createClient();
 
   const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
 
-  const { error } = await supabase.auth.signInWithPassword(data);
+  const parsed = loginSchema.safeParse(data);
+  if (!parsed.success) return { error: parsed.error.issues[0].message };
+
+  const { error } = await supabase.auth.signInWithPassword(parsed.data);
 
   if (error) return { error: error.message };
 
--- src/app/(auth)/signup/action.ts
+++ src/app/(auth)/signup/action.ts
@@ -3,21 +3,22 @@
 import { createClient } from "@/lib/superbase/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signupSchema } from "@/lib/zodSchemas";
 
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
   const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
 
-  const { error } = await supabase.auth.signUp(data);
+  const parsed = signupSchema.safeParse(data);
+  if (!parsed.success) return { error: parsed.error.issues[0].message }; // return first validation error
 
-  if (error) {
-    // Return only if signup fails
-    return { error: error.message };
-  }
+  const { error } = await supabase.auth.signUp(parsed.data);
+
+  if (error) return { error: error.message };
 
   revalidatePath("/", "layout");
   redirect("/profile/create");
